90a13bebcee2ae3a3f1eb33c3713976808d26b88
youngMaster46 <kaplin95@inbox.ru>
2019-07-15 07:55:46 +0400

    cut BLL from App.js in index.js and insert data in App.js througth PROPS

--- src/App.js
+++ src/App.js
@@ -7,35 +7,14 @@ import Dialogs from './components/Dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
 import News from './components/News/News';
 
-const DComponent = () =>  <Dialogs dialogsData={dialogsData} messagesData={messagesData}/>
-const DProfile = () => <Profile postsData = {postsData}/>
-let dialogsData = [
-  {name: 'Max', id: '1'},
-  {name: 'Dimych', id: '2'},
-  {name: 'Vitya', id: '3'},
-  {name: 'Dyadya', id: '4'}
-];
-
-
-let messagesData = [
-  {id: '1', message: 'I am Jack. Nice to meet you, Sandra'},
-  {id: '2', message: 'Glad to see you too, Jack. Are you alone at this party?'},
-  {id: '3', message: 'Yes, I am. My friend has fallen ill today and decided to stay at home. And you?'}
-];
-
-let postsData = [
-  {id: '1', message: 'Hello Max', likesCount: '11'},
-  {id: '2', message: 'You are very strong', likesCount: '4'},
-  {id: '3', message: 'Hey', likesCount: '99'},
-  {id: '4', message: 'Hey', likesCount: '34'},
-  {id: '5', message: 'Hey', likesCount: '66'},
-  {id: '6', message: 'Hey', likesCount: '23'}
-];
 
 
 
 
-function App() {
+function App(props) {git 
+  const DComponent = () =>  <Dialogs dialogsData={props.dialogsData} messagesData={props.messagesData}/>
+const DProfile = () => <Profile postsData = {props.postsData}/>
+
   return (
     <BrowserRouter>
     <div className="app-wrapper">
--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let dialogsData = [
+    {name: 'Max', id: '1'},
+    {name: 'Dimych', id: '2'},
+    {name: 'Vitya', id: '3'},
+    {name: 'Dyadya', id: '4'}
+  ];
+  
+  let messagesData = [
+    {id: '1', message: 'I am Jack. Nice to meet you, Sandra'},
+    {id: '2', message: 'Glad to see you too, Jack. Are you alone at this party?'},
+    {id: '3', message: 'Yes, I am. My friend has fallen ill today and decided to stay at home. And you?'}
+  ];
+  
+  let postsData = [
+    {id: '1', message: 'Hello Max', likesCount: '11'},
+    {id: '2', message: 'You are very strong', likesCount: '4'},
+    {id: '3', message: 'Hey', likesCount: '99'},
+    {id: '4', message: 'Hey', likesCount: '34'},
+    {id: '5', message: 'Hey', likesCount: '66'},
+    {id: '6', message: 'Hey', likesCount: '23'}
+  ];
+  
+
+ReactDOM.render(<App dialogsData={dialogsData} messagesData={messagesData} postsData={postsData} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
