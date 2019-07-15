29b75d79d415f772ea06ebacab43d5a95d137cbf
youngMaster46 <kaplin95@inbox.ru>
2019-07-15 07:35:42 +0400

    Delete BLL from Dialogs.jsx, added it to App.js and pop in props in Dialogs.jsx

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ import Dialogs from './components/Dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
 import News from './components/News/News';
 
+const SomeComponent = () =>  <Dialogs dialogsData={dialogsData} messagesData={messagesData}/>
+
+let dialogsData = [
+  {name: 'Max', id: '1'},
+  {name: 'Dimych', id: '2'},
+  {name: 'Vitya', id: '3'},
+  {name: 'Dyadya', id: '4'}
+];
+
+
+let messagesData = [
+  {id: '1', message: 'I am Jack. Nice to meet you, Sandra'},
+  {id: '2', message: 'Glad to see you too, Jack. Are you alone at this party?'},
+  {id: '3', message: 'Yes, I am. My friend has fallen ill today and decided to stay at home. And you?'}
+];
+
+
+
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,7 +34,7 @@ function App() {
       <Header />
       <Navbar />
      <div className='app-wrapper-content'>
-      <Route component={Dialogs}  path='/dialogs' />
+      <Route render={SomeComponent}  path='/dialogs' />
       <Route component={Profile} path='/profile' />
       <Route component={News} path='/news' />
      </div>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,25 +3,11 @@ import classes from './Dialogs.module.css';
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
 
-let dialogsData = [
-    {name: 'Max', id: '1'},
-    {name: 'Dimych', id: '2'},
-    {name: 'Vitya', id: '3'},
-    {name: 'Dyadya', id: '4'}
-];
-let dialogsElements = dialogsData.map( dialogData => <DialogItem name={dialogData.name} id={dialogData.id}/> );
-
-let messagesData = [
-    {id: '1', message: 'I am Jack. Nice to meet you, Sandra'},
-    {id: '2', message: 'Glad to see you too, Jack. Are you alone at this party?'},
-    {id: '3', message: 'Yes, I am. My friend has fallen ill today and decided to stay at home. And you?'}
-];
-
-let messagesElements = messagesData.map( messageData => <Message message={messageData.message} /> );
-
 
 const Dialogs = (props) => {
-   
+    let dialogsElements = props.dialogsData.map( dialogData => <DialogItem name={dialogData.name} id={dialogData.id}/> );
+    let messagesElements = props.messagesData.map( messageData => <Message message={messageData.message} /> );
+    
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
