92251729b91236652eaad58038498a087bc7d279
youngMaster46 <kaplin95@inbox.ru>
2019-08-09 07:59:08 +0300

    install axios, made request on the server api via get, added image default

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/default-icon.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-    props.setUsers([
-        {id: '1', photoUrl: 'https://million-wallpapers.ru/wallpapers/4/9/370236529480860.jpg',followed: false, fullName: 'Dmitry', status:'I am a boss', location: {city: 'Minsk', country:'Bularus'}},
-        {id: '2', photoUrl: 'https://million-wallpapers.ru/wallpapers/2/40/511464155433370.jpg', followed: true, fullName: 'Oleg', status:'I am a boss too', location: {city: 'Kursk', country:'Russia'}},
-        {id: '3', photoUrl: 'https://avatars.mds.yandex.net/get-pdb/1748217/046a90e7-6d01-4eb7-b619-be8d9a885d7f/s1200', followed: false, fullName: 'Ruslan', status:'Hey, I am a boss', location: {city: 'Kiev', country:'Ukraine'}}
-    ]);
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => { 
+    debugger;    
+    
+    props.setUsers(response.data.items);
+    })
 } 
     return <div>
         {
             props.users.map(u => <div key={u.id}> 
         <span>
             <div>
-                <img className={styles.userPhoto} src={u.photoUrl}/>
+                <img className={styles.userPhoto} src={u.photos.small != null ? u.photos : userPhoto}/>
             </div>
             <div>
                 {u.followed 
@@ -24,11 +27,11 @@ let Users = (props) => {
         </span>
         <span>
             <span>
-                <div>{u.fullName}</div><div>{u.status}</div>
+                <div>{u.name}</div><div>{u.status}</div>
             </span>
             <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                {/* <div>{u.location.country}</div> */}
+                {/* <div>{u.location.city}</div> */}
             </span>
         </span>
         </div>
