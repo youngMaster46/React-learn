7cdd386fd7964a7d1556284e5e03e8dc79bd3a88
youngMaster46 <kaplin95@inbox.ru>
2019-08-12 20:32:01 +0300

    made pagination: added in initialstate pagesize, totalusercount and currentpage, added two constant/actioncreators: setcurrentpageac, settotaluserscountac and case to them. throw up to mapstatetoprops: pagesize, totaluserscount, currentpage. added two dispatch to mapdispatchtoprops: setcurrentpage, settotaluserscount. in users added new method onpagechanged which via onclick give other page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,32 @@ import userPhoto from '../../assets/images/default-icon.png';
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => 
-        this.props.setUsers(response.data.items)
-        );
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => { 
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+        console.log(response.data);
+        
+    });
     }
-    
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => 
+        this.props.setUsers(response.data.items))
+    }
     render() { 
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+        
+
      return <div>
+        {pages.map(p => <span className={this.props.currentPage == p && styles.selectedItems} 
+        onClick={() => {
+            this.onPageChanged(p)
+        }}>{p}</span>)}
     
     {
         this.props.users.map(u => <div key={u.id}> 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Users from './Users';
 import {connect} from 'react-redux';
-import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer';
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage   
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -18,6 +21,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (number) => {
+            dispatch(setCurrentPageAC(number))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
