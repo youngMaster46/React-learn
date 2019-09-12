820776c55cdd15b295ff1a977046fd282038f105
youngMaster46 <kaplin95@inbox.ru>
2019-09-12 20:48:48 +0300

    install react-form, added in state, in redux-store form: formReducer, wrote three <Field> component with name, placeholder, component. I gave my form name is 'login'

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const Login = () => {
-    return <h1> Login </h1>;
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+        return <div><h1>Lalalagin</h1></div>
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    );
 }
+
+const LoginForm = (props) => {
+    return (<form onSubmit={props.handleSubmit}>
+        <div><Field component={'input'} placeholder={'username'} name={'login'} /></div>
+        <div><Field placeholder={'password'} component={'input'} name={'password'} /></div>
+        <div><Field name={'rememberMe'} type={'checkbox'} component={'input'}/></div>
+        <div><button>Submit</button></div>
+    </form>)
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
 
 export default Login;
